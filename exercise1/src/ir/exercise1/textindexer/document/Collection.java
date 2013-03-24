7903f0951902f562ffd17379ef74c2b28074eb24
Florian Eckerstorfer <florian@theroadtojoy.at>
2013-03-24 14:39:03 +0100

    Added data

---
+++ exercise1/src/ir/exercise1/textindexer/document/Collection.java
@@ -0,0 +1,5 @@
+package ir.exercise1.textindexer.document;
+
+class Collection implements CollectionInterface
+{
+}
---
+++ exercise1/src/ir/exercise1/textindexer/document/DocumentInterface.java
@@ -0,0 +1,5 @@
+package ir.exercise1.textindexer.document;
+
+interface DocumentInterface
+{
+}
