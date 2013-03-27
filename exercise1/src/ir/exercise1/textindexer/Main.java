8e79511ca04234dd8a4a3cdb7fbb7d561dcd253f
haichao <haichao@e211-182.eduroam.tuwien.ac.at>
2013-03-27 16:21:27 +0100

    - minor bug in Main class fixed (missing parameter in TextDocumentReader)

--- exercise1/src/ir/exercise1/textindexer/Main.java
+++ exercise1/src/ir/exercise1/textindexer/Main.java
@@ -3,6 +3,7 @@ package ir.exercise1.textindexer;
 import ir.exercise1.textindexer.reader.collection.CollectionReaderInterface;
 import ir.exercise1.textindexer.reader.collection.ClassCollectionReader;
 import ir.exercise1.textindexer.reader.document.TextDocumentReader;
+import ir.exercise1.textindexer.reader.file.FilesystemReader;
 import ir.exercise1.textindexer.document.DocumentInterface;
 import ir.exercise1.textindexer.document.ClassDocument;
 import ir.exercise1.textindexer.document.ClassDocumentFactory;
@@ -20,7 +21,7 @@ class Main
     {
         System.out.println("Let's start by reading files from the file system.");
 
-        CollectionReaderInterface reader = new ClassCollectionReader("./data/20_newsgroups_subset", new TextDocumentReader(new ClassDocumentFactory()));
+        CollectionReaderInterface reader = new ClassCollectionReader("./data/20_newsgroups_subset", new TextDocumentReader(new ClassDocumentFactory(), new FilesystemReader()));
         CollectionInterface collection = reader.read();
         while (collection.hasNext()) {
             ClassDocument doc = (ClassDocument)collection.next();
