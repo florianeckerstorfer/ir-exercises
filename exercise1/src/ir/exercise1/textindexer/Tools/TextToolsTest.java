7e46bff51c315efeae66e4b07a702febac38b14e
Florian Eckerstorfer <florian@theroadtojoy.at>
2013-04-12 15:26:29 +0200

    Fixed TextToolsTest

--- exercise1/src/ir/exercise1/textindexer/Tools/TextToolsTest.java
+++ exercise1/src/ir/exercise1/textindexer/Tools/TextToolsTest.java
@@ -2,6 +2,7 @@ package ir.exercise1.textindexer.Tools;
 
 import static org.junit.Assert.assertEquals;
 import ir.exercise1.textindexer.document.Document;
+import ir.exercise1.textindexer.stemmer.*;
 
 import java.util.ArrayList;
 
@@ -18,30 +19,30 @@ import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public class TextToolsTest {
-	
+
 	@Before
     public void setUp()
 	{
-		
+
 	}
-	
+
 	@Test
 	public void testAll_stopWords() {
-		
+
 		boolean b = TextTools.isStopWord("be");
-		
+
 		assertEquals(true, b);
 	}
-	
-	
+
+
 	@Test
 	public void testDoStemming_Suffix_stripping() {
-		Stemmer stemmer = new Stemmer();
-		
+		StemmerInterface stemmer = new PorterStemmer();
+
 		String s = TextTools.doStemming("university", stemmer);
-		
+
 		assertEquals("univers", s);
 	}
-	
+
 
 }
